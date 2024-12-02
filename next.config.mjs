b8c5bb1f79bb3a739030d349aa4d0033777777df
uid289250 <uid289250@od-8b16a8.infomaniak.ch>
2024-12-02 14:44:29 +0100

    Server version

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,27 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   compiler: {
     styledComponents: true
+  },
+  poweredByHeader: false,
+  serverRuntimeConfig: {
+    // Configuration serveur uniquement
+  },
+  publicRuntimeConfig: {
+    // Configuration serveur et client
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-Forwarded-For',
+            value: '${req.headers["x-forwarded-for"]}'
+          }
+        ]
+      }
+    ];
   }
 };
 
